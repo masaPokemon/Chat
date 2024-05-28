84785ab408cb0cb6a56ee0898d67dbed135fb9bd
masaPokemon <masa.gongon2011@gmail.com>
2024-05-29 08:41:49 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,6 +1,20 @@
 import sqlite3
 import streamlit as st
 
+import login
+
+if 'authentication_status' not in st.session_state:
+    st.session_state['authentication_status'] = None
+
+if __name__ == "__main__":
+    # ログイン認証に成功すれば処理切り替え
+    if st.session_state['authentication_status']:
+        # こにメインのアプリ機能を書く
+        if st.button("ログアウト"):
+            st.session_state['authentication_status'] = None
+            st.experimental_rerun()
+    else:
+        login.Login("db/user.db")
 
 # データベースに接続する
 conn = sqlite3.connect('ChatData.db')
