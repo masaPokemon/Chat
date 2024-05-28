c7859f925dfa521eb2524ab7c457c5ac92cce989
masaPokemon <masa.gongon2011@gmail.com>
2024-05-29 08:54:09 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -7,25 +7,7 @@ if 'authentication_status' not in st.session_state:
 
 if __name__ == "__main__":
     # ログイン認証に成功すれば処理切り替え
-    if st.session_state['authentication_status']:
-        
-        # データベースに接続する
-        conn = sqlite3.connect('ChatData.db')
-        c = conn.cursor()
-
-        # Display chat messages from history on app rerun
-        for message in c:
-            st.markdown(c)
-
-        # React to user input
-        if prompt := st.chat_input("user"):
-            c.execute(user, prompt)
-            conn.commit
-        # こにメインのアプリ機能を書く
-        if st.button("ログアウト"):
-            st.session_state['authentication_status'] = None
-            st.experimental_rerun()
-        
+    if st.session_state['authentication_status']
         # こにメインのアプリ機能を書く
         if st.button("ログアウト"):
             st.session_state['authentication_status'] = None
