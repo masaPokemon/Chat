6b29e716c1b6bf63d70f792b743e1c7141f8acf8
masaPokemon <masa.gongon2011@gmail.com>
2024-05-29 09:24:13 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,8 +1,23 @@
-import sqlite3
 import streamlit as st
+import mysql.connector
+
+### ファイル読み込み
+ 
+### DB接続
+cnx = mysql.connector.connect(host='localhost', user='root', password='', database='user.db')
+cnx2 = mysql.connector.connect(host='localhost', user='root', password='', database='ChatData.db')
+ 
+### カーソル作成
+cursor = cnx.cursor()
+
+### カーソル作成
+cursor2 = cnx.cursor() 
+
+### INSERT文作成
+sql = "INSERT INTO weather_forecast ( date_time, pressure, temperature, humidity) VALUES (%s, %s, %s, %s)"
+
 user = st.text_input("name")
 # データベースに接続する
-conn = sqlite3.connect('ChatData.db')
 c = conn.cursor()
 st.title("Echo Bot")
 
@@ -20,5 +35,14 @@ if prompt := st.chat_input("What is up?"):
     st.chat_message("user").markdown(prompt)
     # Add user message to chat history
     st.session_state.messages.append({"role": user, "content": prompt})
-    c += prompt
+    
+    ### データ挿入実行
+    cursor.execute()
     conn.commit()
+
+
+### コミット
+cnx.commit()
+ 
+### カーソルクローズ
+cursor.close()
