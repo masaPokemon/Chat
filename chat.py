e222ed02178f0b25f85938dd0ccfac5473426975
masaPokemon <masa.gongon2011@gmail.com>
2024-05-29 09:39:13 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,16 +1,4 @@
 import streamlit as st
-
-import main
-import login
-
-
-# 共通のsessin state ログイン情報
-if 'authentication_status' not in st.session_state:
-    st.session_state['authentication_status'] = None
-
-if __name__ == "__main__":
-    # ログイン認証に成功すればmain_appに切り替え
-    if st.session_state['authentication_status']:
-        app.app()
-    else:
-        obj_auth = login.Login("db/user.db")
+import pandas as pd
+import sqlite3 
+import hashlib
