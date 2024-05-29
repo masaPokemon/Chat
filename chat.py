ac9fc70eb3f404b122ff58b943c3209e9bf6037c
masaPokemon <masa.gongon2011@gmail.com>
2024-05-29 09:12:13 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -11,9 +11,8 @@ if "messages" not in st.session_state:
     st.session_state.messages = []
 
 # Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+for message in c:
+    st.markdown(c)
 
 # React to user input
 if prompt := st.chat_input("What is up?"):
