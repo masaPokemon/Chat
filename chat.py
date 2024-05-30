93028d679da45b127e99f8cc46cfa12852022f44
masaPokemon <masa.gongon2011@gmail.com>
2024-05-30 10:58:56 +0900

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,7 +1,17 @@
-import pyopenjtalk
 import numpy as np
-from scipy.io import wavfile
+import pandas as pd
+import sqlite3
+import seaborn as sns
+!pip install streamlit
 
-text = "AI Academyで音声合成を学んでいます"
-x, sr = pyopenjtalk.tts(text)
-wavfile.write("output.wav", sr, x.astype(np.int16))
+from pycaret.datasets import get_data
+#ボストンデータを取得
+df = get_data('boston')
+
+# データベース名とテーブル名
+db_name = 'datasets.db'
+table_name = 'tips'
+
+# SQLiteに書き込む
+with sqlite3.connect(db_name) as conn:
+    df.to_sql(table_name, conn)
