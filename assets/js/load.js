67d3b3f60e7ac35c6a04281e6e8608b0d6667977
masaPokemon <masa.gongon2011@gmail.com>
2025-01-24 12:09:29 +0900

    Create load.js

---
+++ assets/js/load.js
@@ -0,0 +1,11 @@
+let a = document.createElement("script");
+  let b = document.createElement("script");
+  let c = document.createElement("script");
+  let d = document.createElement("script");
+
+  a.src = "assets/js/slider/slider.js";
+  b.src = "assets/js/changelog/recentupd.js";
+  c.src = "assets/js/testimonials/updatetests.js";
+  d.src = "assets/js/partners/updatepartners.js";
+
+  document.body.append(a, b, c, d);
