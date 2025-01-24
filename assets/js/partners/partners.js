6fd72d60b4f6e1952da76b3d6c234a6f2b416c1d
masaPokemon <masa.gongon2011@gmail.com>
2025-01-24 12:15:01 +0900

    Create partners.js

---
+++ assets/js/partners/partners.js
@@ -0,0 +1,8 @@
+const partners = [
+    {
+        name: `WebLFG`,
+        blurb: `From internet classics to present-day favorites, it's online entertainment right at your fingertips.`,
+        link: `weblfg.com`,
+        image: `weblfg.png`
+    },
+]
